55b73540c4d0224bdcd64af8ca01b7e5548cfafe
CarloDettori <dettoric07@gmail.com>
2024-12-11 17:01:12 +0100

    fist time adding react just for try

---
+++ mixed-data/food.js
@@ -0,0 +1,28 @@
+const foods = [
+  {
+    id: 1,
+    name: "Lifelong",
+    category: "Cat",
+    weight: 3000,
+    ingredients: "Pollo e riso",
+    thumb: "img/food/lifelong.jpg",
+  },
+  {
+    id: 2,
+    name: "Trainer",
+    category: "Cat",
+    weight: 10000,
+    ingredients: "Carni bianche",
+    thumb: "img/food/trainer.jpg",
+  },
+  {
+    id: 3,
+    name: "Ultima",
+    category: "Dog",
+    weight: 800,
+    ingredients: "Pollo",
+    thumb: "img/food/ultima.jpg",
+  },
+];
+
+module.exports = foods;
